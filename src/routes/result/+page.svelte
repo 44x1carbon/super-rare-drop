96878008322a5600206690084688ce18e0218512
44 <44x1carbon@gmail.com>
2024-09-15 10:48:24 +0900

    upd

--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -11,7 +11,7 @@
   async function roll10Gacha() {
     eggs = [];
 
-    let isAllLR = 0.05 <= Math.random();
+    let isAllLR = 0.05 >= Math.random();
 
     for (let i = 0; i < 10; i++) {
       await tick();
